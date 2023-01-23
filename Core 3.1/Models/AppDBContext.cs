0002998b1fc6e0b58ffb74001a0d07c8c8a50bc2
Aleyna KIRŞEN <aleynaakrsnn@gmail.com>
2023-01-23 21:13:42 +0300

    Add project files.

---
+++ Core 3.1/Models/AppDBContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Core_3._1.Models
+{
+    public class AppDBContext : IdentityDbContext
+    {
+        private readonly DbContextOptions _options;
+
+        public AppDBContext(DbContextOptions options): base(options)
+        {
+            _options = options; 
+        }
+
+        //VERİTABANI İLK DEFA OLUŞTURULURKEN CALISAN BİR VİRTUAL METOTDUR. bU METOT SAYESİNDE VERİTABANI TABLOLARO OLUSTURMADAN ARAYA GİRECEK TABLO İSİMLERİNE MÜDAHELE EDEBİLCEK  VEYA KOLONLARA  İSTEDİGİMİZ  AYARLARI GERÇEKLEŞTİRMEK İÇİN KULLANILIR.
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+        }
+    }
+}
---
+++ Core 3.1/ViewModel/RegisterViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SLE_System.Models
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+         
+        [DataType(DataType.Password)]
+        [Display(Name ="TEKRAR Password")]
+        [Compare("Password",ErrorMessage ="PaSSWORD EŞLEŞMİYOR.")]
+        public string ConfirmPassword { get; set; }
+    }
+}
